fa1e0969e48aaf26d082ac7057feb762b193ce19
AndreyRGW <RGWyo@ANDREY-PC>
2026-06-06 15:12:31 +0300

    restore stderr.go with proper sync.Mutex (revert no-op debug)

--- experimental/deprecated/stderr.go
+++ experimental/deprecated/stderr.go
@@ -1,8 +1,15 @@
 package deprecated
 
-import "github.com/sagernet/sing/common/logger"
+import (
+	"os"
+	"strconv"
+	"sync"
+
+	"github.com/sagernet/sing/common/logger"
+)
 
 type stderrManager struct {
+	access   sync.Mutex
 	logger   logger.Logger
 	reported map[string]bool
 }
@@ -15,5 +22,25 @@ func NewStderrManager(logger logger.Logger) Manager {
 }
 
 func (f *stderrManager) ReportDeprecated(feature Note) {
-	// no-op: skip deprecated warnings to avoid concurrent map writes
+	f.access.Lock()
+	defer f.access.Unlock()
+	if f.reported[feature.Name] {
+		return
+	}
+	f.reported[feature.Name] = true
+	if !feature.Impending() {
+		f.logger.Warn(feature.MessageWithLink())
+		return
+	}
+	if feature.EnvName != "" {
+		enable, enableErr := strconv.ParseBool(os.Getenv("ENABLE_DEPRECATED_" + feature.EnvName))
+		if enableErr == nil && enable {
+			f.logger.Warn(feature.MessageWithLink())
+			return
+		}
+		f.logger.Error(feature.MessageWithLink())
+		f.logger.Fatal("to continuing using this feature, set environment variable ENABLE_DEPRECATED_" + feature.EnvName + "=true")
+	} else {
+		f.logger.Error(feature.MessageWithLink())
+	}
 }
